351c7b263cce6c87c78b0d614cf82d15be4a06d9
Synxsaints <Synxsaints@LAPTOP-EAMMNJ87>
2019-09-26 14:40:51 +0700

    First Commit

---
+++ src/tugaspbolatihan2/TugasPBOLatihan2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugaspbolatihan2;
+
+/**
+ *
+ * @author 
+ * Nama : Permai Ramadhan
+ * Kelas : IF 2
+ * NIM : 10118060
+ * Deskripsi Program : Deklarasi & Pengaksesan Variabel
+ */
+public class TugasPBOLatihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+        
+    }
+    
+}
